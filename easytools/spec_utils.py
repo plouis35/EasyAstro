ceb339eddbf712d399a02f2c246fe0e5590a59b7
Pascal LOUIS <plouis35@gmail.com>
2024-08-01 18:47:41 +0200

    easyspectrum : normalization added

--- easytools/spec_utils.py
+++ easytools/spec_utils.py
@@ -1,35 +1,17 @@
 import matplotlib.pyplot as plt
 
-def show_lines(z, ax = None, rest_frame = True, em_label = True, abs_label = True, em_lines = None, abs_lines = None):
+def show_lines(ax = None, show_line = True):
     """
-    Function to add emission and/or absorption lines onto a plot. 
-    
-    If em_lines or abs_lines is given, plotting only the specified lines.
-    If no lines are given, plotting all the lines that are present in x-range of the plot.
-    
+    Show lines onto a plot. 
+        
     Parameters
-    ----------
-    z : float
-        Redshift value of the source
-    
+    ----------    
     ax : AxesSubplot
         The axis onto which the emission/absoption lines needs to be plotted.
         If ax = None, then the plotting function uses plt, rather than axis.
         
-    rest_frame : bool
-        Whether or not the plot is in rest-frame. Default is True.
-    
-    em_label : bool
-        Whether or not to label the emission lines. Default is True.
-        
-    abs_label : bool
-        Whether or not to label the absorption lines. Default is True.
-    
-    em_lines : list
-        List of emission lines to label
-    
-    abs_lines : list
-        List of absorption lines to label
+    show_line : bool
+        Whether or not to draw vertical dashed lines. Default is True.
     
     Returns
     -------
@@ -37,115 +19,72 @@ def show_lines(z, ax = None, rest_frame = True, em_label = True, abs_label = Tru
     
     """
     
-    # List of lines
-    # This is the set of emission lines from the spZline files. 
-    # All the wavelengths are in vaccuum wavelengths.
-
-    # Emission Lines
-    emission_lines = [
-    {"name" : "Ly-alpha",       "lambda" : 1215.67,  "emission": True,  "label" : "Ly$\\alpha$"},
-    {"name" : "N V 1240",       "lambda" : 1240.81,  "emission": True,  "label" : "N V"},
-    {"name" : "C IV 1549",      "lambda" : 1549.48,  "emission": True,  "label" : "C IV" },
-    {"name" : "He II 1640",     "lambda" : 1640.42,  "emission": True,  "label" : "He II"},
-    {"name" : "C III] 1908",    "lambda" : 1908.734, "emission": True,  "label" : "C III]"},
-    {"name" : "Mg II 2799",     "lambda" : 2800.315, "emission": True,  "label" : "Mg II" },
-    {"name" : "[O II] 3725",    "lambda" : 3727.092, "emission": True,  "label" : " "},
-    {"name" : "[O II] 3727",    "lambda" : 3729.875, "emission": True,  "label" : "[O II]"}, 
-    {"name" : "[Ne III] 3868",  "lambda" : 3869.857, "emission": True,  "label" : "[Ne III]"},
-    {"name" : "H-zeta",         "lambda" : 3890.151, "emission": True,  "label" : "H$\\zeta$"},
-    {"name" : "[Ne III] 3970",  "lambda" : 3971.123, "emission": True,  "label" : "[Ne III]"},
-    {"name" : "H-epsilon",      "lambda" : 3971.195, "emission": True,  "label" : "H$\\epsilon$"}, 
-    {"name" : "H-delta",        "lambda" : 4102.892, "emission": True,  "label" : "H$\\delta$"},
-    {"name" : "H-gamma",        "lambda" : 4341.684, "emission": True,  "label" : "H$\\gamma$"},
-    {"name" : "[O III] 4363",   "lambda" : 4364.435, "emission": True,  "label" : "[O III]"},
-    {"name" : "He II 4685",     "lambda" : 4686.991, "emission": True,  "label" : "He II"},
-    {"name" : "H-beta",         "lambda" : 4862.683, "emission": True,  "label" : "H$\\beta$"},
-    {"name" : "[O III] 4959",   "lambda" : 4960.294, "emission": True,  "label" : "[O III]" },
-    {"name" : "[O III] 5007",   "lambda" : 5008.239, "emission": True,  "label" : "[O III]" },
-    {"name" : "He II 5411",     "lambda" : 5413.025, "emission": True,  "label" : "He II"},
-    {"name" : "[O I] 5577",     "lambda" : 5578.888, "emission": True,  "label" : "[O I]" },
-    {"name" : "[N II] 5755",    "lambda" : 5756.186, "emission": True,  "label" : "[Ne II]" },
-    {"name" : "He I 5876",      "lambda" : 5877.308, "emission": True,  "label" : "He I" },
-    {"name" : "[O I] 6300",     "lambda" : 6302.046, "emission": True,  "label" : "[O I]" },
-    {"name" : "[S III] 6312",   "lambda" : 6313.806, "emission": True,  "label" : "[S III]" },
-    {"name" : "[O I] 6363",     "lambda" : 6365.535, "emission": True,  "label" : "[O I]" },
-    {"name" : "[N II] 6548",    "lambda" : 6549.859, "emission": True,  "label" : "[N II]" },
-    {"name" : "H-alpha",        "lambda" : 6564.614, "emission": True,  "label" : "H$\\alpha$" },
-    {"name" : "[N II] 6583",    "lambda" : 6585.268, "emission": True,  "label" : "[N II]" },
-    {"name" : "[S II] 6716",    "lambda" : 6718.294, "emission": True,  "label" : "[S II]" },
-    {"name" : "[S II] 6730",    "lambda" : 6732.678, "emission": True,  "label" : "[S II]" },
-    {"name" : "[Ar III] 7135",  "lambda" : 7137.758, "emission": True,  "label" : "[Ar III]" },]
-
-
-    # Absorption lines
-    absorption_lines = [
-#    {"name" : "H12",            "lambda" : 3751.22,  "emission": False, "label" : "H12"},
-#    {"name" : "H11",            "lambda" : 3771.70,  "emission": False, "label" : "H11"},
-#    {"name" : "H10",            "lambda" : 3798.98,  "emission": False, "label" : "H10"},
-#    {"name" : "H9",             "lambda" : 3836.48,  "emission": False, "label" : "H9"},
-    {"name" : "H-zeta",         "lambda" : 3890.151, "emission": False, "label" : "H$\\zeta$" },
-    {"name" : "K (Ca II 3933)", "lambda" : 3934.814, "emission": False, "label" : "K (Ca II)"},
-    {"name" : "H (Ca II 3968)", "lambda" : 3969.623, "emission": False, "label" : "H (Ca II)"},
-    {"name" : "H-epsilon",      "lambda" : 3971.195, "emission": False, "label" : "H$\\epsilon$"}, 
-    {"name" : "H-delta",        "lambda" : 4102.892, "emission": False, "label" : "H$\\delta$" },
-    {"name" : "G (Ca I 4307)",  "lambda" : 4308.952, "emission": False, "label" : "G (Ca I)"},
-    {"name" : "H-gamma",        "lambda" : 4341.684, "emission": False, "label" : "H$\\gamma$"},
-    {"name" : "H-beta",         "lambda" : 4862.683, "emission": False, "label" : "H$\\beta$"},
-    {"name" : "Mg I 5183",      "lambda" : 5185.048, "emission": False, "label" : ""},    #triplet high res
-    {"name" : "Mg I 5172",      "lambda" : 5174.125, "emission": False, "label" : "Mg I"},
-    {"name" : "Mg I 5167",      "lambda" : 5168.762, "emission": False, "label" : ""},
-    {"name" : "D2 (Na I 5889)", "lambda" : 5891.582, "emission": False, "label" : "D2 (Na I)" },
-    {"name" : "D1 (Na I 5895)", "lambda" : 5897.554, "emission": False, "label" : "D1 (Na I)" },
-    {"name" : "H-alpha",        "lambda" : 6564.614, "emission": False, "label" : "H$\\alpha$"},
-    {"name" : "B (atm O2 6867-6944)", "lambda" : 6867.0, "emission": False, "label" : "atm (O2)"},
-    {"name" : "H2O (atm 7168-7394)", "lambda" : 7168.0, "emission": False, "label" : "atm (H2O)"},
-    {"name" : "A (atm O2 7594-7684)", "lambda" : 7584.0, "emission": False, "label" : "atm (O2)"},
+    lines_to_display = [
+        {"name" : 'Zero Order',"label" :'Zero', "lambda" :0.00}, 
+        {"name" : 'Hydrogen', "label" : 'Hα',  "lambda" :656.2852}, 
+        {"name" : 'Hydrogen', "label" : 'Hβ',  "lambda" :486.133 },
+        {"name" : 'Hydrogen', "label" : 'Hγ',  "lambda" :434.047}, 
+        {"name" : 'Hydrogen', "label" : 'Hδ',  "lambda" :410.174}, 
+        {"name" : 'Hydrogen', "label" : 'Hε',  "lambda" :397.007}, 
+        {"name" : 'Hydrogen', "label" : 'Hζ',  "lambda" :388.9049}, 
+        {"name" : 'Hydrogen', "label" : 'Hη',  "lambda" :383.5384}, 
+        {"name" : 'Hydrogen', "label" : 'Hθ',  "lambda" :379.75} , 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :527.04}, 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :516.89}, 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :495.76}, 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :466.81}, 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :438.36}, 
+        {"name" : 'Iron', "label" : 'Fe',  "lambda" :430.79}, 
+        {"name" : 'Magnesium', "label" : 'MgII',  "lambda" :448.11}, 
+        {"name" : 'Magnesium', "label" : 'Mg',  "lambda" :518.36}, 
+        {"name" : 'Magnesium', "label" : 'Mg',  "lambda" :517.27}, 
+        {"name" : 'Magnesium', "label" : 'Mg',  "lambda" :516.73}, 
+        {"name" : 'Neon', "label" : 'NeI',  "lambda" :585.249}, 
+        {"name" : 'Neon', "label" : 'NeI',  "lambda" :588.189}, 
+        #{"name" : 'Mercury', "label" : 'Hg',  "lambda" :404.656}, 
+        #{"name" : 'Mercury', "label" : 'Hg',  "lambda" :435.833}, 
+        #{"name" : 'Mercury', "label" : 'Hg',  "lambda" :546.074}, 
+        #{"name" : 'Mercury', "label" : 'Hg',  "lambda" :576.960}, 
+        #{"name" : 'Mercury', "label" : 'Hg',  "lambda" :578.966}, 
+        {"name" : 'Sodium', "label" : 'NaI',  "lambda" :589.00}, 
+        {"name" : 'Sodium', "label" : 'NaI',  "lambda" :589.59}, 
+        {"name" : 'Oxygen', "label" : 'O1',  "lambda" :615.82}, 
+        {"name" : 'Oxygen', "label" : 'O2',  "lambda" :627.77}, 
+        {"name" : 'Oxygen', "label" : 'O2',  "lambda" :686.9}, 
+        {"name" : 'Oxygen', "label" : 'O2',  "lambda" :718.6}, 
+        {"name" : 'Oxygen', "label" : 'O2',  "lambda" :760.5}, 
+        {"name" : 'Oxygen', "label" : 'O2',  "lambda" :898.77}, 
+        {"name" : 'Oxygen', "label" : 'OIII',  "lambda" :495.9}, 
+        {"name" : 'Oxygen', "label" : 'OIII',  "lambda" :500.69}, 
+        {"name" : 'Water', "label" : 'H2O',  "lambda" :651.65}, 
+        {"name" : 'Water', "label" : 'H2O',  "lambda" :694.07}, 
+        {"name" : 'Water', "label" : 'H2O',  "lambda" :695.64}, 
+        {"name" : 'Water', "label" : 'H2O',  "lambda" :698.90}, 
+        {"name" : 'Calcium', "label" : 'Ca+ H',  "lambda" :396.85}, 
+        {"name" : 'Calcium', "label" : 'Ca+ K',  "lambda" :393.37}, 
+        {"name" : 'Helium', "label" : 'He I',  "lambda" :706.52}, 
+        {"name" : 'Helium', "label" : 'He I',  "lambda" :667.82}, 
+        {"name" : 'Helium', "label" : 'He I',  "lambda" :587.56}, 
+        {"name" : 'Helium', "label" : 'He I',  "lambda" :501.57}, 
+        {"name" : 'Helium', "label" : 'He I',  "lambda" :447.148}, 
+        {"name" : 'Silicon', "label" : 'Si II',  "lambda" :634.71}, 
+        {"name" : 'Silicon', "label" : 'Si II',  "lambda" :637.14}, 
+        {"name" : 'Terbium', "label" : 'Tb',  "lambda" :487.7}, 
+        {"name" : 'Terbium', "label" : 'Tb',  "lambda" :542.4}, 
+        {"name" : 'Europium', "label" : 'Eu',  "lambda" :611.6}, 
+        {"name" : 'Titanium', "label" : 'Ti+',  "lambda" :336.11}
     ]
-    
+
     if (ax == None):
-        # If there is no axes given, plotting with the plt function
         ax = plt.gca()
-    
-    if (em_lines != None):
-        # Choosing the emission lines listed by the user
-        emission_lines = list(filter(lambda x: x['name'] in em_lines, emission_lines))
-    
-    if (abs_lines != None):
-        # Choosing the absorption lines listed by the user
-        absorption_lines = list(filter(lambda x: x['name'] in abs_lines, absorption_lines)) 
-    
-    xbounds = ax.get_xbound()   # Getting the x-range of the plot 
-    # This is for selecting only those lines that are visible in the x-range of the plot
-    
-    for ii in range(len(emission_lines)):
-        # If rest_frame = False, redshifting the emission lines to the observed frame of the source
-        if (rest_frame == False):
-            lam = emission_lines[ii]['lambda']*(1+z)
-        else:
-            lam = emission_lines[ii]['lambda']
-        # Plotting the emission lines if they are within the x-range of the plot
-        if (em_label == True):
-            if (emission_lines[ii]['emission']) & (lam > xbounds[0]) & (lam < xbounds[1]):
-                ax.axvline(lam, 0.95, 1.0, color = 'b', lw = 1.0)
-                ax.axvline(lam, color = 'b', lw = 1.0, linestyle = ':')
-                trans = ax.get_xaxis_transform()
-                # Labeling the emission lines if em_label = True
-                ax.annotate(emission_lines[ii]['label'], xy = (lam, 1.05), xycoords = trans, \
-                         fontsize = 8, rotation = 90, color = 'b')
-            
-    for ii in range(len(absorption_lines)):
-        # If rest_frame = False, redshifting the absorption lines to the observed frame of the source
-        if (rest_frame == False):
-            lam = absorption_lines[ii]['lambda']*(1+z)
-        else:
-            lam = absorption_lines[ii]['lambda']
-        # Plotting the absorption lines if they are within the x-range of the plot
+        
+    xbounds = ax.get_xbound()   # Getting the x-range of the plot     
+    for ii in range(len(lines_to_display)):
+        lam = lines_to_display[ii]['lambda'] * 10    # nm to AA
         if (lam > xbounds[0]) & (lam < xbounds[1]):
-            if (abs_label == True):
-                ax.axvline(lam, 0.95, 1.0, color = 'r', lw = 1.0)
-                ax.axvline(lam, color = 'r', lw = 1.0, linestyle = ':')
-    #            ax.axvline(lam, 0.2, 1.0, color = 'r', lw = 1.0, linestyle = ':')
-                trans = ax.get_xaxis_transform()
-                # Labeling the absorption lines if abs_label = True
-                ax.annotate(absorption_lines[ii]['label'], xy = (lam, 1.01), xycoords = trans, \
-                         fontsize = 6, rotation = 90, color = 'r')
+            ax.axvline(lam, 0.95, 1.0, color = 'red', lw = 1.0)
+            ax.axvline(lam, color = 'red', lw = 0.3, linestyle = ':')
+            trans = ax.get_xaxis_transform()
+            ax.annotate(lines_to_display[ii]['label'], xy = (lam, 1.05), xycoords = trans, \
+                     fontsize = 8, rotation = 90, color = 'red')
+
